7bcd57e0b4277e9f9b83128f3cafe0ec8c34d2d9
Chax0180 <chaxbent1324@gmail.com>
2025-09-02 10:18:49 -0400

    Update ModTeams.java

--- content/ModTeams.java
+++ content/ModTeams.java
@@ -3,11 +3,10 @@ import mindustry.game.Team;
 import mindustry.mod.Mod;
 
 public class hldstry extends Mod {
-    public static Team orange;
 
     @Override
     public void init() {
-        insects = new Team(7, "insects", Color.valueOf("ff8800"));
+        insects = new Team(7, "insects", Color.valueOf("487bc2"));
         androids = new Team(8, "androids", Color.valueOf("ff0000"));
         luminaries = new Team(9, "luminaries", Color.valueOf("6b3aba"));
         "super serpulo" = new Team(10, "super serpulo", Color.valueOf("41639C"));
