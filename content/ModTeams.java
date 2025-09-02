c20c9c4de92093ea98116def7c207baff065706a
Chax0180 <chaxbent1324@gmail.com>
2025-09-02 10:16:51 -0400

    Create ModTeams.java

---
+++ content/ModTeams.java
@@ -0,0 +1,15 @@
+import arc.graphics.Color;
+import mindustry.game.Team;
+import mindustry.mod.Mod;
+
+public class hldstry extends Mod {
+    public static Team orange;
+
+    @Override
+    public void init() {
+        insects = new Team(7, "insects", Color.valueOf("ff8800"));
+        androids = new Team(8, "androids", Color.valueOf("ff0000"));
+        luminaries = new Team(9, "luminaries", Color.valueOf("6b3aba"));
+        "super serpulo" = new Team(10, "super serpulo", Color.valueOf("41639C"));
+    }
+}
